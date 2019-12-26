53d57fecc1caf165c86cbb5a1159eafabc341a24
rutuls <rutulshah2007@gmail.com>
2019-12-26 19:16:45 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import React from 'react';
 function App() {
   return (
     <div>
+      <div className="header">
       Phone Directory
+      </div>
+      <button>Add</button>
+      <div>
+        <span>Name</span><br/>
+        <span>Phone</span>
+      </div>
     </div>
   );
 }
