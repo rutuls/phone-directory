ba66e477b06fdeaa121208318c6bfa7ea55337f5
rutuls <rutulshah2007@gmail.com>
2020-01-05 17:27:37 +0530

    Added state in ‘AddSubscriber’ and ‘App’ components

--- src/AddSubscribers.js
+++ src/AddSubscribers.js
@@ -3,7 +3,25 @@ import Header from './Header';
 import './AddSubscribers.css';
 import './common/common.css';
 class AddSubscribers extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            id: 0,
+            name: '',
+            phone: '',
+        }
+    }
+
+    inputChangedHandler = (e) => {
+        const state = this.state;
+        state[e.target.name] = e.target.value;
+        this.setState(state);
+        console.log(this.state);
+    }
+
     render() {
+        const p = this.state.phone;
         return (
             <div>
                 <Header heading="Add Subscribers"></Header>
@@ -11,15 +29,15 @@ class AddSubscribers extends Component {
                 <button className="custom-btn">Back</button>
                 <form className="subscriber-form">
                     <label htmlFor="name" className="label-control">Name: </label><br/>
-                    <input id="name" type="text" className="input-control" name="name"></input><br/><br/>
+                    <input id="name" type="text" className="input-control" name="name" onChange={this.inputChangedHandler}></input><br/><br/>
 
                     <label htmlFor="phone" className="label-control">Phone: </label><br/>
-                    <input id="phone" type="text" className = "input-control" name="phone"></input>
+                    <input id="phone" type="text" className = "input-control" name="phone" onChange={this.inputChangedHandler}></input>
 
                     <div className="subscriber-info-container">
                         <span className="subscriber-to-add-heading"> Subscriber to be added: </span><br/>
-                        <span className="subscriber-info">Name: </span><br/>
-                        <span className="subscriber-info">Phone: </span><br/>
+                        <span className="subscriber-info">Name: {this.state.name}</span><br/>
+                        <span className="subscriber-info">Phone: {p} </span><br/>
                         <span></span>
                     </div>
                     <button type="submit" className="custom-btn add-btn">Add</button>
--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@ import React, { Component } from 'react';
 import Header from "./Header";
 import './App.css'
 class App extends Component {
+
+
+  constructor() {
+    super();
+    this.state={
+      subscribersListToShow: []
+    };
+  }
+
   render() {
-    let subscribers = [
-      {
-        id: '101',
-        name: 'Rutul Shah',
-        phone: '888111888'
-      },
-      {
-        id: '102',
-        name: 'Radhika Mandil',
-        phone: '999111999'
-      }
-    ];
+    // let subscribers = [
+    //   {
+    //     id: '101',
+    //     name: 'Rutul Shah',
+    //     phone: '888111888'
+    //   },
+    //   {
+    //     id: '102',
+    //     name: 'Radhika Mandil',
+    //     phone: '999111999'
+    //   }
+    // ];
+
     return (
       <div>
       <Header heading="Phone Directory"></Header>
@@ -26,7 +36,7 @@ class App extends Component {
         </div>
 
         {
-          subscribers.map(sub => <div key={sub.id} className="grid-container">
+          this.state.subscribersListToShow.map(sub => <div key={sub.id} className="grid-container">
           <span className="grid-item">{sub.name}</span>
           <span className="grid-item">{sub.phone}</span>
           <span className="grid-item action-btn-container"><button className="custom-btn delete-btn">Delete</button></span>
